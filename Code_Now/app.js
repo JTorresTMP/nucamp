1a61d006d5e165bdd41811dd50d22e2668fe6cd4
Jose Torres <jose.torres@ultra.me>
2020-09-04 13:56:57 -0700

    added JQuery for icon spacing, smooth now

--- Code_Now/app.js
+++ Code_Now/app.js
@@ -1,6 +1,6 @@
-//:sweating:
 
 
+//NAV JQUERY
 $(document).ready(function() {
     let alter_m_nav = function() {
         let vw = document.body.clientWidth;
@@ -27,3 +27,36 @@ $(document).ready(function() {
     alter_m_nav();
 
 }); 
+
+//SIGN UP ICONS
+
+$(document).ready(function() {
+    let alter_icon_spacing = function() {
+        let vw = document.body.clientWidth;
+
+        if (vw <= 340) {
+            $('#googleIcon').removeClass('mr-2');
+            $('#appleIcon').removeClass('mr-2');
+            $('#githubIcon').removeClass('mr-2');
+            $('#linkedinIcon').removeClass('mr-2');
+            $('#googleIcon').addClass('mr-1');
+            $('#appleIcon').addClass('mr-1');
+            $('#githubIcon').addClass('mr-1');
+            $('#linkedinIcon').addClass('mr-1');
+        } else {
+            $('#googleIcon').removeClass('mr-1');
+            $('#appleIcon').removeClass('mr-1');
+            $('#githubIcon').removeClass('mr-1');
+            $('#linkedinIcon').removeClass('mr-1');
+            $('#googleIcon').addClass('mr-2');
+            $('#appleIcon').addClass('mr-2');
+            $('#githubIcon').addClass('mr-2');
+            $('#linkedinIcon').addClass('mr-2');
+        }
+    };
+    $(window).resize(function() {
+        alter_icon_spacing();
+    })
+
+    alter_icon_spacing();
+})
