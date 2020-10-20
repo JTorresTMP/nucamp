76c2c94cae79a28c2a450275779d53321e3921f0
Jose Torres <jose.torres@ultra.me>
2020-10-20 10:32:24 -0700

    added directory route, need to double check what CampsiteWithID is doing

--- 3-React/nucampsite1010/src/components/functionalApproach/MainComp.js
+++ 3-React/nucampsite1010/src/components/functionalApproach/MainComp.js
@@ -8,6 +8,9 @@ import { PARTNERS } from '../../shared/partners'
 import { PROMOTIONS } from '../../shared/promotions'
 import Header from './HeaderComp'
 import Footer from './FooterComp'
+import Home from './HomeComp'
+import Contact from './ContactComp'
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 const Main = (props) => {
     const [campsites, setCampsites] = useState(CAMPSITES)
@@ -21,19 +24,45 @@ const Main = (props) => {
     }
 
     const HomePage = () => {
-        return <div></div>
+        return (
+            <div>
+                <Home 
+                campsite={campsites
+                    .filter(campsite => campsite.featured)[0]}
+                promotion={promotions
+                    .filter(promotion => promotion.featured)[0]}
+                partner={partners
+                    .filter(partner => partner.featured)[0]}/>
+            </div>
+        )
     }
 
-    const CampsiteWithID = () => {
-        return <div></div>
+    //Need to double check to see what this function is actually doing
+    const CampsiteWithID = ({match}) => {
+        return (
+            <div>
+                <CampsiteInfo
+                    campsite={campsites.filter(campsite => {
+                        return campsite.id === +match.args.campsiteId
+                    })[0]}
+                    comments={comments.filter(comment => {
+                        return comment.campsiteId === +match.args.campsiteId
+                    })}
+                />
+            </div>
+        )
     }
 
     return (
         <div>
-            {/* <NavbarComp /> */}
             <Header />
-            <Directory campsites={campsites} 
-            onClick={campsiteID => onCampsiteSelect(campsiteID.id)} />
+            <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/directory' 
+                    render={() => <Directory campsites={campsites} />} />
+                <Route exact path='/contactus' component={Contact} />
+                <Route path='/directory/:campsiteId' component={CampsiteWithID} />
+            </Switch>
             <Footer />
         </div>
     )
