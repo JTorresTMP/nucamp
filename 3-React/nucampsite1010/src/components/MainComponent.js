1dd5c35ddc60c86657dc88959eba3b975a78e197
Jose Torres <jose.torres@ultra.me>
2020-10-19 20:33:13 -0700

    adding to the single page app

--- 3-React/nucampsite1010/src/components/MainComponent.js
+++ 3-React/nucampsite1010/src/components/MainComponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import DirectoryCLS from './DirectoryComponent';
 import CampsiteInfoCLS from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites'
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent';
+import Contact from './ContactComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,6 +16,9 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS,
             selectedCampsite: null
         };
     }
@@ -23,7 +30,13 @@ class Main extends Component {
     render() {
 
         const HomePage = () => {
-            return <Home />
+            return (
+                <Home
+                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            )
         }
         // console.log('In state', this.state.selectedCampsite)
         // console.log('Filtered:', this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite))
@@ -33,6 +46,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <DirectoryCLS campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 {/* <DirectoryCLS campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
