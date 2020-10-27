b4fb7b827aadceb1d9bdb3279ff23ef689d0510a
Jose Torres <torresjose6541@gmail.com>
2020-10-26 19:33:13 -0700

    intro to redux exercise

--- 3-React/nucampsite1010/src/App.js
+++ 3-React/nucampsite1010/src/App.js
@@ -3,18 +3,24 @@ import './App.css';
 import MainCLS from './components/MainComponent'
 import Main from './components/functionalApproach/Main'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ConfigureStore } from './redux/configureStore'
+
+const store = ConfigureStore();
 
 class App extends React.Component {
   render() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h4 className="text-center">
-          You are viewing the Class Implementation
-        </h4>
-        <MainCLS />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <h4 className="text-center">
+            You are viewing the Class Implementation
+          </h4>
+          <MainCLS />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
   }
 }
--- 3-React/nucampsite1010/src/components/MainComponent.js
+++ 3-React/nucampsite1010/src/components/MainComponent.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 import DirectoryCLS from './DirectoryComponent';
 import CampsiteInfoCLS from './CampsiteInfoComponent';
-import { CAMPSITES } from '../shared/campsites'
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent';
 import Contact from './ContactComponent'
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import About from './AboutComponent'
+import { connect } from 'react-redux'
 
-
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners,
+        promotions: state.promotions
+    };
+};
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            partners: PARTNERS,
-            promotions: PROMOTIONS,
-            selectedCampsite: null
-        };
-    }
-
     onCampsiteSelect(campsiteId) {
         this.setState({selectedCampsite: campsiteId});
     }
@@ -35,21 +28,18 @@ class Main extends Component {
         const HomePage = () => {
             return (
                 <Home
-                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                partner={this.state.partners.filter(partner => partner.featured)[0]}
-                />
-            )
-        }
-        const CampsiteWithId = ({match}) => {
-            console.log('match', match)
-            return (
-                <CampsiteInfoCLS 
-                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                    campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.props.partners.filter(partner => partner.featured)[0]}
                 />
             );
-        };  
+        };
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfoCLS campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                  comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+            );
+        }; 
         // console.log('In state', this.state.selectedCampsite)
         // console.log('Filtered:', this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite))
         return (
@@ -57,10 +47,10 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <DirectoryCLS campsites={this.state.campsites} />} />
-                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/directory' render={() => <DirectoryCLS campsites={this.props.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/about' render={() => <About partners={this.state.partners} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
@@ -69,4 +59,4 @@ class Main extends Component {
     };
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
