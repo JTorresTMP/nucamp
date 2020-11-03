8a1eea1e8a8f55b6ee502db58146fe42488caa2f
Jose Torres <torresjose6541@gmail.com>
2020-11-02 20:11:58 -0800

    fetch post a comment

--- 3-React/nucampsite1010/src/components/MainComponent.js
+++ 3-React/nucampsite1010/src/components/MainComponent.js
@@ -9,7 +9,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import About from './AboutComponent'
 import { connect } from 'react-redux'
 import { 
-    addComment, 
+    postComment, 
     fetchCampsites, 
     fetchComments, 
     fetchPromotions } 
@@ -17,14 +17,13 @@ from '../redux/ActionCreators'
 import { actions } from 'react-redux-form';
 
 const mapDispatchToProps = {
-    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
     fetchCampsites: () => (fetchCampsites()),
     resetFeedbackForm: () => (actions.reset('feedbackForm')),
     fetchComments: () => (fetchComments()),
     fetchPromotions: () => (fetchPromotions())
 }
 
-console.log('Import definition', addComment)
 
 const mapStateToProps = state => {
     return {
@@ -71,7 +70,7 @@ class Main extends Component {
                     errMess={this.props.campsites.errMess}
                     comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                     commentsErrMess={this.props.comments.errMess}
-                    addComment={this.props.addComment} />    
+                    postComment={this.props.postComment} />    
             );
         }; 
         // console.log('In state', this.state.selectedCampsite)
