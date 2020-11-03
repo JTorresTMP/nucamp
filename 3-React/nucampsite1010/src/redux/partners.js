fb357053cf9824692241a0e12ec1c91446506fa1
Jose Torres <torresjose6541@gmail.com>
2020-11-03 11:32:57 -0800

    added cases to partners js, still need logic. Also fixed spacing for promotions.js

--- 3-React/nucampsite1010/src/redux/partners.js
+++ 3-React/nucampsite1010/src/redux/partners.js
@@ -3,6 +3,12 @@ import * as ActionTypes from './ActionTypes'
 
 export const Partners = (state = PARTNERS, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_PARTNERS:
+            return state;
+        case ActionTypes.PARTNERS_LOADING:
+            return state;
+        case ActionTypes.PARTNERS_FAILED:
+            return state;
         default:
             return state;
     }
