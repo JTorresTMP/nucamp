1842c01d998f0ddbed5bbaf05f0c686b10d65e4f
Jose Torres <torresjose6541@gmail.com>
2020-11-03 11:19:43 -0800

    modeled partners.js after promotions and comments

--- 3-React/nucampsite1010/src/redux/partners.js
+++ 3-React/nucampsite1010/src/redux/partners.js
@@ -1,8 +1,16 @@
 import { PARTNERS } from '../shared/partners'
 import * as ActionTypes from './ActionTypes'
 
-export const Partners = (state = PARTNERS, action) => {
+export const Partners = (state = { isLoading: true, errMsg: null, partners: []}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_PARTNERS:
+            return {...state, isLoading: false, errMsg: null, partners: action.payload}
+
+        case ActionTypes.PARTNERS_FAILED:
+            return {...state, isLoading: false, errMsg: action.payload};
+
+        case ActionTypes.PARTNERS_LOADING:
+            return {...state, isLoading: true, errMsg: null, partners: []}
         default:
             return state;
     }
