73b0d157954e4a0d8811152c13970f2e17e821c6
Jose Torres <torresjose6541@gmail.com>
2020-12-26 14:53:52 -0800

    app and rectangle using es6 imports

---
+++ 5-NodeJS-Express-MongoDB/node-examplesES6/app.mjs
@@ -0,0 +1,25 @@
+import { rect } from './rectangle.mjs';
+
+const solveRect = (l, w) => {
+    console.log(`Solving for rectangle dimensions: ${l}, ${w}`);
+
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message);
+        } else {
+            console.log(
+                `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
+            );
+            console.log(
+                `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
+            );
+        }
+    });
+
+    console.log('This is run after');
+};
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
