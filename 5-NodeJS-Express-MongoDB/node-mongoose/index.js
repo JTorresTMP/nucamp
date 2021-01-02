cc2aa31aeb2cf2ad0985dd38ab03caa068225a96
Jose Torres <torresjose6541@gmail.com>
2021-01-02 12:25:14 -0800

    mongoose part 2

--- 5-NodeJS-Express-MongoDB/node-mongoose/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -9,31 +9,26 @@ const connect = mongoose.connect(url, {
     useUnifiedTopology: true
 });
 
-connect
-    .then(() => {
-        console.log('Connected correctly to server');
+connect.then(() => {
+    console.log('Connected correctly to server');
 
-        const newCampsite = new Campsite({
-            name: 'React Lake Campground',
-            description: 'test'
-        });
-
-        newCampsite
-            .save()
-            .then((campsite) => {
-                console.log(campsite);
-                return Campsite.find();
-            })
-            .then((campsites) => {
-                console.log(campsites);
-                return Campsite.deleteMany();
-            })
-            .then(() => {
-                return mongoose.connection.close();
-            })
-            .catch((err) => {
-                console.log(err);
-                mongoose.connection.close();
-            });
+    Campsite.create({
+        name: 'React Lake Campground',
+        description: 'test'
     })
-    .catch((error) => console.log(error));
+        .then((campsite) => {
+            console.log(campsite);
+            return Campsite.find();
+        })
+        .then((campsites) => {
+            console.log(campsites);
+            return Campsite.deleteMany();
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+            mongoose.connection.close();
+        });
+});
