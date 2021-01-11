8876cef7e7b3c8c94261b37d6781c91dbf25ce19
Jose Torres <torresjose6541@gmail.com>
2021-01-11 10:35:57 -0800

    basic authentication

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/app.js
@@ -27,6 +27,31 @@ connect.then(
     (err) => console.log(err)
 );
 
+const auth = (req, res, next) => {
+    console.log(req.headers);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        const err = new Error('You are not authenticated');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64')
+        .toString()
+        .split(':');
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+        return next(); //User is authorized
+    } else {
+        const err = new Error('You are not authenticated');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -35,6 +60,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Auth
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
