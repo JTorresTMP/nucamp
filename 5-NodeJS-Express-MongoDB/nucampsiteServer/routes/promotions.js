b2ac10ae5724f3721520f3fc9917a14eaaeacf97
Jose Torres <torresjose6541@gmail.com>
2021-01-16 11:26:20 -0800

    task 1 and task 2 complete

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/promotions.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/promotions.js
@@ -13,29 +13,37 @@ promotionsRouter
             })
             .catch((err) => next(err));
     })
-    .post(authenticate.verifyUser, (req, res, next) => {
-        Promotion.create(req.body)
-            .then((promotion) => {
-                console.log('Promotion has been created: ', promotion);
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(promotion);
-            })
-            .catch((err) => next(err));
-    })
+    .post(
+        authenticate.verifyUser,
+        authenticate.verifyAdmin,
+        (req, res, next) => {
+            Promotion.create(req.body)
+                .then((promotion) => {
+                    console.log('Promotion has been created: ', promotion);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(promotion);
+                })
+                .catch((err) => next(err));
+        }
+    )
     .put(authenticate.verifyUser, (req, res) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /promotions');
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
-        Promotion.deleteMany()
-            .then((response) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(response);
-            })
-            .catch((err) => next(err));
-    });
+    .delete(
+        authenticate.verifyUser,
+        authenticate.verifyAdmin,
+        (req, res, next) => {
+            Promotion.deleteMany()
+                .then((response) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(response);
+                })
+                .catch((err) => next(err));
+        }
+    );
 
 promotionsRouter
     .route('/:promotionId')
@@ -52,29 +60,37 @@ promotionsRouter
             `POST operation not supported on /promotions/${req.params.promotionId}`
         );
     })
-    .put(authenticate.verifyUser, (req, res, next) => {
-        Promotion.findByIdAndUpdate(
-            req.params.promotionId,
-            {
-                $set: req.body
-            },
-            { new: true }
-        )
-            .then((promotion) => {
-                res.setHeader('Content-Type', 'application/json');
-                res.status(200).json(promotion);
-            })
-            .catch((err) => next(err));
-    })
-    .delete(authenticate.verifyUser, async (req, res, next) => {
-        try {
-            const response = await Promotion.findByIdAndDelete(
-                req.params.promotionId
-            );
-            res.status(200).json(response);
-        } catch (err) {
-            next(err);
+    .put(
+        authenticate.verifyUser,
+        authenticate.verifyAdmin,
+        (req, res, next) => {
+            Promotion.findByIdAndUpdate(
+                req.params.promotionId,
+                {
+                    $set: req.body
+                },
+                { new: true }
+            )
+                .then((promotion) => {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(200).json(promotion);
+                })
+                .catch((err) => next(err));
         }
-    });
+    )
+    .delete(
+        authenticate.verifyUser,
+        authenticate.verifyAdmin,
+        async (req, res, next) => {
+            try {
+                const response = await Promotion.findByIdAndDelete(
+                    req.params.promotionId
+                );
+                res.status(200).json(response);
+            } catch (err) {
+                next(err);
+            }
+        }
+    );
 
 module.exports = promotionsRouter;
